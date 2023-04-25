78f0467838d3258dab0cdd6ba278a8217b74b816
Islem Belhadef <islem.belhadef.dev@gmail.com>
2023-04-25 12:17:00 +0100

    idk what i added

--- src/app/authSlice.js
+++ src/app/authSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
   isAuthenticated: true,
-  type: "student",
+  type: "supervisor",
 }
 
 export const authSlice = createSlice({

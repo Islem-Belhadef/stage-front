72974bd46404a8b34124327175e94bde72060256
ashraf med <achrafdms10@gmail.com>
2023-05-03 12:02:13 +0100

    fix superAdmin dashboard and make it responsive

--- src/pages/SuperAdminDashboard.jsx
+++ src/pages/SuperAdminDashboard.jsx
@@ -3,9 +3,15 @@ import Header from "../components/Header"
 import Account from "../components/Account"
 
 // Assets
-import { SearchNormal1 } from "iconsax-react"
+import { SearchNormal1, Setting5 } from "iconsax-react"
+import AccountsStats from "../components/AcountsStats"
+import { useState } from "react"
+import AccountsFilter from "../components/AccountsFilter"
 
 const SuperAdminDashboard = () => {
+
+  const [showFilters, setshowFilters] = useState(false)
+
   const accounts = [
     {
       firstName: "Mohamed Islem",
@@ -34,86 +40,97 @@ const SuperAdminDashboard = () => {
   ]
 
   return (
-    <div className="bg-gray-50">
+    <div className="bg-bg">
       <Header fontColor="#272937" bgColor="#FFFFFF" btnColor="#383EBE" />
-      <div className="container mx-auto flex flex-col lg:flex-row gap-8 py-10">
-        <div className="flex flex-col gap-8">
-          <div className="bg-white rounded-xl shadow-md shadow-gray-100 py-10 px-6 h-fit">
-            <h1 className="font-header text-text text-2xl font-bold">Search</h1>
-            <form className="flex flex-col items-center mt-6">
-              <div className="flex flex-col items-end w-full">
+      <div className="container mx-auto flex flex-col gap-8 py-10">
+        <AccountsStats />
+        <div className="flex flex-col lg:flex-row gap-8">
+          <div className="hidden lg:flex flex-col gap-8">
+            <div className="bg-white rounded-xl shadow-md shadow-gray-100 py-10 px-6 h-fit">
+              <h1 className="font-header text-text text-2xl font-bold">Search</h1>
+              <form className="flex flex-col items-center mt-6">
+                <div className="flex flex-col items-end w-full">
+                  <input
+                    type="text"
+                    name="type"
+                    id="type"
+                    className="input mt-2 w-full pr-10"
+                    placeholder="Search by name.."
+                  />
+                  <SearchNormal1
+                    color="#CED3E1"
+                    size={20}
+                    style={{
+                      position: "relative",
+                      left: "-12px",
+                      top: "-38px",
+                      cursor: "pointer",
+                    }}
+                  />
+                </div>
+                <button type="submit" className="primary-btn">
+                  Search
+                </button>
+              </form>
+            </div>
+            <div className="bg-white rounded-xl shadow-md shadow-gray-100 py-10 px-6 h-fit">
+              <h1 className="font-header text-text text-2xl font-bold">
+                Filters
+              </h1>
+              <AccountsFilter />
+            </div>
+          </div>
+          {accounts.length > 0 && (
+            <div className="bg-white rounded-xl shadow-md flex-1 h-fit">
+              {/* for filter and search in small screens */}
+              <div className="flex lg:hidden items-center rounded-t-xl w-full h-fit px-5 bg-white border-b border-lightGray">
                 <input
                   type="text"
                   name="type"
                   id="type"
-                  className="input mt-2 w-full"
+                  className="input my-2 w-full pr-10 border-none bg-[#EEF0F4]"
                   placeholder="Search by name.."
                 />
                 <SearchNormal1
                   color="#CED3E1"
-                  size={20}
-                  style={{
-                    position: "relative",
-                    left: "-12px",
-                    top: "-38px",
-                    cursor: "pointer",
-                  }}
+                  size={30}
+                  className=" relative -left-9 cursor-pointer"
+
                 />
+                <button className="flex items-center gap-1 font-medium text-text text-xl"
+                  onClick={() => { setshowFilters(true) }}
+
+                >
+                  <Setting5 />
+                  filter
+                </button>
               </div>
-              <button type="submit" className="primary-btn">
-                Search
+
+              {/*  */}
+              {accounts.map((account) => {
+                return <Account account={account} key={account.email} />
+              })}
+            </div>
+          )}
+        </div>
+      </div>
+      {showFilters && (
+        <div className="fixed bg-black bg-opacity-80 inset-0 flex  items-center">
+          <div className="w-full sm:w-3/4 max-w-xl bg-white rounded-xl  py-7 px-6 h-fit mx-auto">
+            <div className="flex items-center justify-between">
+              <h1 className="font-header text-text text-2xl font-bold">
+                Filters
+              </h1>
+              <button
+                onClick={() => { setshowFilters(false) }}
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" width="1.7rem" height="1.7rem" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
               </button>
-            </form>
-          </div>
-          <div className="bg-white rounded-xl shadow-md shadow-gray-100 py-10 px-6 h-fit">
-            <h1 className="font-header text-text text-2xl font-bold">
-              Filters
-            </h1>
-            <form className="flex flex-col items-center mt-6">
-              <label htmlFor="type" className="label mb-4">
-                Account type
-                <select
-                  name="type"
-                  id="type"
-                  className="input mt-2 bg-transparent"
-                >
-                  <option value="">Any</option>
-                  <option value="student">Student</option>
-                  <option value="supervisor">Supervisor</option>
-                  <option value="hod">Head of Department</option>
-                </select>
-              </label>
-              <label htmlFor="order" className="label mb-4">
-                Order by
-                <select
-                  name="order"
-                  id="order"
-                  className="input mt-2 bg-transparent"
-                >
-                  <option value="">creation date (ascending)</option>
-                  <option value="">creation date (descending)</option>
-                  <option value="">account type</option>
-                </select>
-              </label>
-              <div className="flex gap-4 mt-2">
-                <button type="reset" className="secondary-btn">
-                  Reset
-                </button>
-                <button type="submit" className="primary-btn">
-                  Filter
-                </button>
-              </div>
-            </form>
+            </div>
+            <AccountsFilter />
           </div>
         </div>
-        {accounts.length > 0 && (
-          <div className="bg-white rounded-xl shadow-md flex-1">
-            {accounts.map((account) => {
-              return <Account account={account} key={account.email} />
-            })}
-          </div>
-        )}
-      </div>
+      )}
     </div>
   )
 }

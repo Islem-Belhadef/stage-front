2fc4b792b842b6b4ca695516af52e3541ca53fcb
ashraf med <achrafdms10@gmail.com>
2023-05-03 12:04:09 +0100

    made offers page responsive with filters

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -4,111 +4,73 @@ import Offer from "../components/Offer"
 
 // Assets
 import emptyBox from "../assets/empty-box.svg"
+import { Setting5 } from "iconsax-react"
+import OffersFilter from "../components/OffersFilter"
+
 //data
 import offers from "../offersData"
+import { useState } from "react"
+
 
 const Offers = () => {
+
+  const [showFilters, setshowFilters] = useState(false)
+
   return (
-    <div className="bg-gray-50">
+    <div className="bg-gray-50 h-[100svh]">
       <Header fontColor="#272937" bgColor="#FFFFFF" btnColor="#383EBE" />
       <div className="container mx-auto flex flex-col lg:flex-row gap-8 py-10">
-        <div className="bg-white rounded-xl shadow-md shadow-gray-100 py-10 px-6 h-fit">
+
+        <div className="hidden lg:block sticky top-10 bg-white rounded-xl shadow-md shadow-gray-100 py-10 px-6 h-fit">
           <h1 className="font-header text-text text-2xl font-bold">Filters</h1>
-          <form className="flex flex-col items-center mt-6">
-            <label htmlFor="duration" className="label mb-4">
-              Internship duration
-              <div className="flex gap-4 mt-2">
-                <input
-                  type="number"
-                  className="input"
-                  placeholder="Minimum"
-                  min={0}
-                  style={{ width: "9rem" }}
-                />
-                <input
-                  type="number"
-                  className="input"
-                  placeholder="Maximum"
-                  min={0}
-                  style={{ width: "9rem" }}
-                />
-              </div>
-            </label>
-            <label htmlFor="spots" className="label mb-4">
-              Available spots
-              <div className="flex gap-4 mt-2">
-                <input
-                  type="number"
-                  className="input"
-                  placeholder="Minimum"
-                  min={0}
-                  style={{ width: "9rem" }}
-                />
-                <input
-                  type="number"
-                  className="input"
-                  placeholder="Maximum"
-                  min={0}
-                  style={{ width: "9rem" }}
-                />
-              </div>
-            </label>
-            <label htmlFor="level" className="label mb-4">
-              Level
-              <select
-                name="level"
-                id="level"
-                className="input mt-2 bg-transparent"
-              >
-                <option value="">Any</option>
-                <option value="L-3">L-3</option>
-                <option value="M-2">M-2</option>
-              </select>
-            </label>
-            <label htmlFor="order" className="label mb-4">
-              Order by
-              <select
-                name="order"
-                id="order"
-                className="input mt-2 bg-transparent"
-              >
-                <option value="">Offer date (ascending)</option>
-                <option value="">Offer date (descending)</option>
-                <option value="">Internship duration (ascending)</option>
-                <option value="">Internship duration (descending)</option>
-              </select>
-            </label>
-            <div className="flex gap-4 mt-2">
-              <button type="reset" className="secondary-btn">
-                Reset
-              </button>
-              <button type="submit" className="primary-btn">
-                Filter
-              </button>
-            </div>
-          </form>
+          <OffersFilter />
         </div>
         {offers.length > 0 && (
           <div className="bg-white rounded-xl shadow-md flex-1">
+            <div className="flex justify-between items-center px-6 py-4 border-b border-b-gray-200">
+              <p className=" font-header font-medium text-grayText  ">Showing 25 offers</p>
+              <button className="lg:hidden flex items-center gap-1 font-medium text-text text-xl"
+                onClick={() => { setshowFilters(true) }}
+              >
+                <Setting5 />
+                filter
+              </button>
+            </div>
             {offers.map((offer) => {
               return <Offer offer={offer} key={offer.title} />
             })}
           </div>
         )}
         {offers.length === 0 && (
-          <div className="bg-white rounded-xl shadow-md flex-1 pb-10 px-10 flex flex-col justify-center items-center">
-            <img src={emptyBox} alt="empty box" className="w-2/3" />
-            <h1 className="font-header text-text text-2xl font-bold mb-4">
+          <div className="bg-white rounded-xl flex-1 pt-10 sm:pt-0 pb-10 px-10 flex flex-col justify-center items-center">
+            <img src={emptyBox} alt="empty box" className="w-2/3 min-w-[15rem]" />
+            <h1 className="font-header text-text text-xl sm:text-2xl font-bold mb-4">
               Nothing
             </h1>
             <p className="font-body text-lightText">
               There are no corresponding internship offers with the filters you
               entered, try removing filters
             </p>
-            <p></p>
           </div>
         )}
       </div>
+
+      {showFilters && (
+        <div className="fixed bg-black bg-opacity-80 inset-0 flex  items-center">
+          <div className="w-full sm:w-3/4 max-w-xl bg-white rounded-xl py-7 px-6 h-fit mx-auto">
+            <div className="flex items-center justify-between">
+              <h1 className="font-header text-text text-2xl font-bold">Filters</h1>
+              <button
+                onClick={() => { setshowFilters(false) }}
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" width="1.7rem" height="1.7rem" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+              </button>
+            </div>
+            <OffersFilter />
+          </div>
+        </div>
+      )}
+
     </div>
   )
 }

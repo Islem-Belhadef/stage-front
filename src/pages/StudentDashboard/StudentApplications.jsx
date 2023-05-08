6cad0678a999aa45b4adb2c69b0c5bb43c306dc7
ashraf med <achrafdms10@gmail.com>
2023-05-08 19:05:01 +0100

    recreate student dashboard and its pages

---
+++ src/pages/StudentDashboard/StudentApplications.jsx
@@ -0,0 +1,75 @@
+import ApplicationsStats from "../../components/ApplicationsStats"
+import Application from "../../components/Application"
+
+const StudentApplications = ()=>{
+
+    const applications = [
+        {
+          student:"Damous Achraf",
+          title: "software engineer",
+          company: "sonatrach",
+          duration: "30 days",
+          status: 0,
+          created_at: "05 may 2023",
+          level: "license 3"
+        },
+        {
+          student:"Belhadef Islem",
+          title: "mobile developer",
+          company: "air algerie",
+          duration: "30 days",
+          status: 0,
+          created_at: "2 months ago",
+          level: "license 3"
+        },
+        {
+          student:"Ghanem Akram",
+          title: "web developer ",
+          company: "mobilis",
+          duration: "15 days",
+          status: 4,
+          created_at: "10 april 2023",
+          level: "license 3"
+        },
+    
+      ]
+
+
+    return(
+        <div className="flex flex-col lg:flex-row gap-8">
+        <div className="flex flex-col gap-8">
+          <ApplicationsStats showButton={true} />
+          
+        </div>
+        {applications.length > 0 && (
+          <div className="bg-white rounded-xl shadow-md flex-1">
+
+           
+            {applications.map((application) => {
+              return <Application application={application} key={application.title} />
+            })}
+          </div>
+        )}
+
+        {applications.length == 0 && (
+          <div className="bg-white rounded-xl shadow-lg shadow-gray-200 pb-10 px-10 flex flex-col justify-center items-center">
+            <img src={emptyBox} alt="empty box" className="w-2/3" />
+            <h1 className="font-header text-text text-2xl font-bold mb-4">
+              Nothing
+            </h1>
+            <p className="font-body text-lightText px-20">
+              You don't have any internship applications just yet,
+              <Link to="/internship/offers" className="text-primary">
+                check our internship offers
+              </Link>
+            </p>
+          </div>
+        )}
+
+
+
+      </div>
+    )
+}
+
+export default StudentApplications
---
+++ src/pages/StudentDashboard/StudentDemands.jsx
@@ -0,0 +1,78 @@
+// Assets
+import emptyBox from "../../assets/empty-box.svg"
+import DemandStats from "../../components/DemandStats"
+import Demand from "../../components/Demand"
+
+
+const StudentDemands = ()=>{
+
+
+  const demands = [
+    {
+      student:"Damous achraf" ,
+      title: "Network technician",
+      company: "ooredoo",
+      duration: "30 days",
+      status: 0,
+      created_at: "05 may 2023",
+      level:"master 2"
+    },
+    {
+      student:"Belhadef Islem" ,
+      title: "ui/ux designer",
+      company: "yassir",
+      duration: "30 days",
+      status: 3,
+      created_at: "yesterday",
+      level: "license 3"
+    },
+    {
+      student:"Belhadef Islem" ,
+      title: "web developer ",
+      company: "mobilis",
+      duration: "30 days",
+      status: 4,
+      created_at: "15 may 2023",
+      level: "license 3"
+    },
+
+  ]
+
+    return(
+        <div className="flex flex-col lg:flex-row gap-8">
+        <div className="flex flex-col gap-8">
+          <DemandStats showButton={true} />
+          
+        </div>
+        {demands.length > 0 && (
+          <div className="bg-white rounded-xl shadow-md flex-1">
+
+           
+            {demands.map((demand) => {
+              return <Demand demand={demand} key={demand.title} />
+            })}
+          </div>
+        )}
+
+        {demands.length == 0 && (
+          <div className="bg-white rounded-xl shadow-lg shadow-gray-200 pb-10 px-10 flex flex-col justify-center items-center">
+            <img src={emptyBox} alt="empty box" className="w-2/3" />
+            <h1 className="font-header text-text text-2xl font-bold mb-4">
+              Nothing
+            </h1>
+            <p className="font-body text-lightText px-20">
+              You don't have any internship demands just yet, try{" "}
+              <Link to="/internship/demand" className="text-primary">
+                adding a new demand
+              </Link>
+            </p>
+          </div>
+        )}
+
+
+
+      </div>
+    )
+}
+
+export default StudentDemands

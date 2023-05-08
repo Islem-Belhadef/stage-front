7afb4926f325886943e26af98b2f2bd895214a71
ashraf med <achrafdms10@gmail.com>
2023-05-08 19:09:09 +0100

    created HOD dashboard and its pages

---
+++ src/pages/HODDashboard/HODDashboard.jsx
@@ -0,0 +1,37 @@
+// React & Router
+import {NavLink, Outlet } from "react-router-dom"
+// Components
+import Header from "../../components/Header"
+
+
+const HODDashboard = () => {
+  return (
+    <div className="bg-gray-50">
+    <Header fontColor="#272937" bgColor="#FFFFFF" btnColor="#383EBE" />
+    <div className="container mx-auto flex flex-col gap-3 py-6">
+      <div className="tabs bg-white rounded-xl w-fit h-fit">
+        <nav className="flex gap-5 items-center px-1 py-1">
+          <NavLink
+          to={"mydemands"}
+            className={({ isActive }) => `${isActive? "text-primary bg-primary bg-opacity-10":"text-lightText"} font-medium font-header px-4 py-2 rounded-lg`}
+          >
+            demands
+          </NavLink>
+          <NavLink
+            to={"myapplications"}
+            className={({ isActive }) => `${isActive? "text-primary bg-primary bg-opacity-10":"text-lightText"} font-medium font-header px-4 py-2 rounded-lg`}>
+            applications
+          </NavLink>
+
+        </nav>
+      </div>
+      
+      
+      <Outlet/>
+      
+    </div>
+  </div>
+  )
+}
+
+export default HODDashboard

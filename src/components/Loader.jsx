5718a42c30aa55fe8dcff91408ad38953d0c2771
ashraf med <achrafdms10@gmail.com>
2023-05-23 23:09:19 +0100

    created new loaders

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,31 +1,48 @@
-const Loader = () => {
+const Loader = ({ layout }) => {
 
     return (
-        <div className="flex flex-col p-6 border-b border-b-gray-200 gap-6 animate-pulse">
-            
-                <div className="flex gap-5">
-                    <div className=" w-32 sm:w-1/5 h-16 bg-gray-200 rounded-xl ">
+        <>
+            {(layout == 'grid')
+                ?
+                (<div className="grid grid-cols-3 gap-x-2 md:gap-x-12 gap-y-4 w-full md:w-4/5 animate-pulse mb-4 ">
+                    
+                    <div className=" h-6 rounded-lg bg-gray-200"></div>
+                    <div className=" h-6 rounded-lg col-span-2 bg-gray-200"></div>
+
+                </div>)
+                :
+              (layout == 'line')
+              ?
+              <div className="w-full max-w-[20rem] px-2 animate-pulse">
+                <div className=" h-8 rounded-lg bg-gray-200"></div>
+              </div>
+              :
+                <div className="flex flex-col p-6 border-b border-b-gray-200 gap-6 animate-pulse">
+
+                    <div className="flex gap-5">
+                        <div className=" w-32 sm:w-1/5 h-16 bg-gray-200 rounded-xl ">
+                        </div>
+                        <div className="w-full flex flex-col gap-2">
+                            <div className=" w-[85%] sm:w-[40%] h-5 bg-gray-200 rounded-xl ">
+                            </div>
+
+                            <div className=" w-24 h-3 bg-gray-200 rounded-lg ">
+                            </div>
+                        </div>
 
                     </div>
-                    <div className="w-full flex flex-col gap-2">
-                        <div className=" w-[85%] sm:w-[40%] h-5 bg-gray-200 rounded-xl ">
-                            
+                    <div className="flex gap-6 items-center">
+                        <div className=" w-3/4 h-3 bg-gray-200 rounded-xl ">
+
                         </div>
-                        <div className=" w-24 h-3 bg-gray-200 rounded-lg ">
-                            
-                        </div>
-                        
                     </div>
+
                 </div>
-                <div className="flex gap-6 items-center">
-                    <div className=" w-3/4 h-3 bg-gray-200 rounded-xl ">
-                        
-                    </div>
-                   
-                   
-                </div>
-        
-        </div>
+            }
+          
+        </>
+
+
     )
 }
 

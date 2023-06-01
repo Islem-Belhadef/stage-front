f093ca62dcee0215fd125e625af3d294d728509b
ashraf med <achrafdms10@gmail.com>
2023-06-01 17:12:51 +0100

    added delete modal to accounts and offers

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,17 +1,22 @@
+import { useState } from "react"
+
 // Assets
-import { Edit, Category } from "iconsax-react"
+import { Edit, Category, Trash } from "iconsax-react"
 import studentImage from "../assets/student.svg"
 import supervisorImage from "../assets/supervisor.svg"
 import hodImage from "../assets/hod.svg"
+import DeleteModal from "./DeleteModal"
 
 const Account = ({ account }) => {
+
+  const [showDelete, setShowDelete] = useState(false)
 
   let img =
     account.role == 0
       ? studentImage
       : account.role == 1
-      ? hodImage
-      : supervisorImage
+        ? hodImage
+        : supervisorImage
 
   return (
     <div className="flex p-6 border-b border-b-gray-200 cursor-pointer hover:bg-gray-200/10">
@@ -31,17 +36,26 @@ const Account = ({ account }) => {
         <div className="flex gap-6 items-center text-lightText font-medium mt-4">
           <p className="flex gap-2 items-center">
             <Category color="#9D9CAC" size={18} />
-            {account.role == 0 ? "student":account.role==1 ? "Head of department" : "supervisor"}
+            {account.role == 0 ? "student" : account.role == 1 ? "Head of department" : "supervisor"}
           </p>
         </div>
         <div></div>
       </div>
-      <div className="flex items-center justify-center">
+      <div className="flex flex-col sm:flex-row gap-5 items-center justify-center">
         <Edit
-          color="#383EBE"
+          color="#7CDF64"
           className="cursor-pointer hover:scale-110 transition"
         />
+        <Trash
+          color="#F64A4A"
+          className="cursor-pointer hover:scale-110 transition"
+          onClick={() => setShowDelete(true)}
+        />
       </div>
+
+      {showDelete &&
+        <DeleteModal For='account' accountId={account.id} accountName={account.last_name +' '+account.first_name} setShowDelete={setShowDelete} />
+      }
     </div>
   )
 }
